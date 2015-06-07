58fbfc418a3dc0bf50334814e7af41eb2ad40b7b
Joe Cabezas Campos <joe.cabezas@gmail.com>
2015-06-07 00:32:19 -0700

    completing twittersend code, now working with media

--- TwitterSender.rb
+++ TwitterSender.rb
@@ -1,13 +1,40 @@
 require 'twitter'
 require './Inbox.rb'
 require './TwitterJob.rb'
+require './Config.rb'
 
 class TwitterSender
 
-	def check
+	def self.check
 		#get job from inbox
 		job = Inbox.peek or return
 
-		
+		#try to send	
+		begin
+			client.update_with_media(job.message, open(job.media_url))
+		rescue
+			#could not send twitter
+			puts "failed: #{$!}"
+			return
+		end
+
+		#twitter sent, delete from pending jobs
+		Inbox.dequeue
+	end
+	
+	private
+
+	@client = nil
+	def self.client
+		if @client.nil?
+			@client = Twitter::REST::Client.new do |config|
+				config.consumer_key        = Config.data[:twitter][:consumer_key]
+				config.consumer_secret     = Config.data[:twitter][:consumer_secret]
+				config.access_token        = Config.data[:twitter][:access_token]
+				config.access_token_secret = Config.data[:twitter][:access_token_secret]
+			end
+		else
+			@client
+		end
 	end
 end
