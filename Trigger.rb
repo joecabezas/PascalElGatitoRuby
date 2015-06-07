d2a8d7bb67a7b5a3046bdbcdf7371f5441a36a1d
Joe Cabezas Campos <joe.cabezas@gmail.com>
2015-06-07 02:37:44 -0700

    now trigger calls twittersender

--- Trigger.rb
+++ Trigger.rb
@@ -1,10 +1,12 @@
 require './RandomPhrases.rb'
 require './Inbox.rb'
+require './TwitterSender.rb'
 
 class Trigger
 
 	def self.trigger
 		Inbox.enqueue(message, media_url)
+		TwitterSender.check
 	end
 
 	private
