792b64232fe9a3a2a9510189765b9537ac1bfe6b
Joe Cabezas Campos <joe.cabezas@gmail.com>
2015-06-07 00:41:39 -0700

    code cleaning

--- Trigger.rb
+++ Trigger.rb
@@ -4,18 +4,15 @@ require './Inbox.rb'
 class Trigger
 
 	def self.trigger
-		message = get_message
-		media_url = get_media_url
-
 		Inbox.enqueue(message, media_url)
 	end
 
 	private
-	def self.get_message
+	def self.message
 		RandomPhrases.get
 	end
 
-	def self.get_media_url
+	def self.media_url
 		'image.gif'
 	end
 end
